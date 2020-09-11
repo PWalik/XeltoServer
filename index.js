0433472ceb07eeacd4e431e0ab01bded38f18538
Walik <patrykwalikwalicki@gmail.com>
2020-09-11 20:06:56 +0200

    solve of CORS (prob needs to change)

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/list', function(req, res) {
+
+    res.header ('Access-Control-Allow-Origin', '*');
+    res.header ('Access-Control-Allow-Credentials', true);
+    res.header ('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header ('Access-Control-Allow-Headers', 'Content-Type');
+
 const sql = require("mssql/msnodesqlv8");
 
 const config = {
@@ -20,9 +26,7 @@ sql.connect(config, function(err) {
     let sqlQuery = 'Select * From dbo.LogHeader WHERE Id < 10';
     sqlRequest.query(sqlQuery, function(err, data) {
         if (err) console.error(err);
-
-        res.send(data);
-
+        res.send(JSON.stringify(data));
         sql.close();
     });
 });
